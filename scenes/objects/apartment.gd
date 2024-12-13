b323e836d9439e619a46ab77daf24a008964b70c
CrumblingAway <antonkid@gmail.com>
2024-12-13 23:36:23 +0200

    FloorLevel: Add basic FloorLevel,Apartment classes.

---
+++ scenes/objects/apartment.gd
@@ -0,0 +1,14 @@
+class_name Apartment extends Node2D
+
+########## Fields. ##########
+
+########## Apartment methods. ##########
+
+########## Node2D methods. ##########
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
---
+++ scenes/objects/floor_level.gd
@@ -0,0 +1,21 @@
+class_name FloorLevel extends Node2D
+
+########## Fields. ##########
+
+var _apartment_to_tile_dict : Dictionary
+
+var _floor_above : FloorLevel
+var _floor_below : FloorLevel
+
+########## FloorLevel methods. ##########
+
+
+
+########## Node2D methods. ##########
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
