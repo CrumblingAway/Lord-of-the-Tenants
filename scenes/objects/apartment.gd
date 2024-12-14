f6de95f3a5a0bb45bf96e33ec802767a9e2adbe5
CrumblingAway <antonkid@gmail.com>
2024-12-14 13:23:03 +0200

    Flow: Outline tenant placement.

--- scenes/objects/apartment.gd
+++ scenes/objects/apartment.gd
@@ -2,9 +2,18 @@ class_name Apartment extends Node2D
 
 ########## Fields. ##########
 
-var _tenants : Array[Tenant]
+var tenant : Tenant:
+	get:
+		return tenant
+	set(new_tenant):
+		tenant = new_tenant
+
+var _tiles : Array[Vector2i]
 
 ########## Apartment methods. ##########
+
+func contains_tile_position(tile_position: Vector2i) -> bool:
+	return _tiles.has(tile_position)
 
 ########## Node2D methods. ##########
 
--- scenes/objects/floor_level.gd
+++ scenes/objects/floor_level.gd
@@ -1,15 +1,36 @@
 class_name FloorLevel extends Node2D
 
+########## Signals. ##########
+
+signal tenant_apartment_mismatch(reasons: Array[String])
+signal tenant_placed_successfully
+
 ########## Fields. ##########
 
-var _apartment_to_tile_dict : Dictionary
+var _apartments : Array[Apartment]
 
 var _floor_above : FloorLevel
 var _floor_below : FloorLevel
 
 ########## FloorLevel methods. ##########
 
+func get_apartment_at_tile_position(tile_position: Vector2i) -> Apartment:
+	for apartment in _apartments:
+		if apartment.contains_tile_position(tile_position):
+			return apartment
+	return null
 
+func place_tenant_in_apartment(tenant: Tenant, apartment: Apartment) -> bool:
+	if not _is_apartment_fit_for_tenant(apartment, tenant):
+		return false
+	
+	apartment.tenant = tenant
+	tenant_placed_successfully.emit()
+	
+	return true
+
+func _is_apartment_fit_for_tenant(apartment: Apartment, tenant: Tenant) -> bool:
+	return true
 
 ########## Node2D methods. ##########
 
