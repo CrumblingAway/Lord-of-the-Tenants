c48851db2d54a46e471a8f91e9356fd8759f0a87
CrumblingAway <antonkid@gmail.com>
2024-12-16 01:33:08 +0200

    FloorLevel: Add highlight impl.

--- scenes/objects/floor_level.gd
+++ scenes/objects/floor_level.gd
@@ -32,8 +32,41 @@ func highlight_apartment_at_tile_position(tile_position: Vector2i) -> void:
 	var apartment : Apartment = get_apartment_at_tile_position(tile_position)
 	if not apartment:
 		return
+	var apartment_tiles : Array[Vector2i] = apartment.tiles
 	
+	# Bounding lines.
+	var left   : int = apartment_tiles.reduce(func(a, b): return min(a.x, b.x))
+	var right  : int = apartment_tiles.reduce(func(a, b): return max(a.x, b.x))
+	var top    : int = apartment_tiles.reduce(func(a, b): return min(a.y, b.y))
+	var bottom : int = apartment_tiles.reduce(func(a, b): return max(a.y, b.y))
 	
+	# Surrounding tiles.
+	var surrounding_tiles : Array[Vector2i]
+	for x in range(left - 1, right + 1):
+		for y in range(top - 1, bottom + 1):
+			if (apartment_tiles.has(Vector2i(x + 1, y)) \
+			   or apartment_tiles.has(Vector2i(x, y + 1))) \
+			   and not apartment_tiles.has(Vector2i(x, y)):
+				surrounding_tiles.push_back(Vector2i(x, y))
+	for x in range(left - 1, right + 1):
+		if apartment_tiles.has(Vector2i(x, bottom)):
+			surrounding_tiles.push_back(Vector2i(x, bottom + 1))
+	for y in range(top - 1, bottom + 1):
+		if apartment_tiles.has(Vector2i(right, y)):
+			surrounding_tiles.push_back(Vector2i(right + 1, y))
+	if apartment_tiles.has(Vector2i(left, top)):
+		surrounding_tiles.push_back(Vector2i(left - 1, top - 1))
+	if apartment_tiles.has(Vector2i(left, bottom)):
+		surrounding_tiles.push_back(Vector2i(left - 1, bottom + 1))
+	if apartment_tiles.has(Vector2i(right, top)):
+		surrounding_tiles.push_back(Vector2i(right + 1, top - 1))
+	if apartment_tiles.has(Vector2i(right, bottom)):
+		surrounding_tiles.push_back(Vector2i(right + 1, bottom + 1))
+	
+	_tilemap.set_cells_terrain_connect(1, surrounding_tiles, 0, 0)
+	
+func clear_highlight() -> void:
+	pass
 
 func place_tenant_in_apartment(tenant: Tenant, apartment: Apartment) -> bool:
 	if not _is_apartment_fit_for_tenant(apartment, tenant):
