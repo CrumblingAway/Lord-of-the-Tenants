010465eb6f9e92d7ea6beb52ace674c7715eaae2
CrumblingAway <antonkid@gmail.com>
2024-12-16 13:41:43 +0200

    Test: Call highlight in level creation test.

--- test/test_floorlevel_create.gd
+++ test/test_floorlevel_create.gd
@@ -7,9 +7,10 @@ var floor_level : FloorLevel
 func run() -> void:
 	floor_level.init(5, 5)
 	floor_level.register_tiles_as_apartment(
-		[Vector2i(1, 2),
-		Vector2i(1, 3)]
+		[Vector2i(2, 2),
+		Vector2i(2, 3)]
 	)
+	floor_level.highlight_apartment_at_tile_position(Vector2i(2, 2))
 
 func get_test_name() -> String:
 	return "Create FloorLevel"
