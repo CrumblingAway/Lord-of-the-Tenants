06e27a354ef0b3ae772787f4ac32b821da6035bb
CrumblingAway <antonkid@gmail.com>
2024-12-17 22:03:11 +0200

    InputManager: Outline tile reservation flow.

--- autoloads/input_manager.gd
+++ autoloads/input_manager.gd
@@ -1,38 +1,79 @@
 extends Node
 
+########## Fields. ##########
+
 enum Mode
 {
 	MENU,
-	LEVEL,
+	LEVEL_IDLE,
+	LEVEL_SELECTING_TILES,
 	COUNT,
 }
-
-########## Fields. ##########
-
-var _mode : Mode = Mode.COUNT
+var _mode : Mode = Mode.COUNT:
+	set(mode):
+		_mode = mode
+		print_mode()
 
 var _floor_level : FloorLevel
 
 ########## InputManager methods. ##########
 
 func _process_level_input() -> bool:
+	if not _floor_level:
+		return false
+	
 	var mouse_position : Vector2 = get_viewport().get_mouse_position()
 	
-	if _floor_level:
-		_floor_level.highlight_apartment_at_global_position(mouse_position)
+	match _mode:
+		Mode.LEVEL_IDLE:
+			_floor_level.highlight_apartment_at_global_position(mouse_position)
+			if Input.is_action_just_pressed("middle_click"):
+				_mode = Mode.LEVEL_SELECTING_TILES
+		Mode.LEVEL_SELECTING_TILES:
+			_floor_level.highlight_reserved_tiles()
+			if Input.is_action_just_pressed("left_click"):
+				if _floor_level.is_tile_at_global_position_available(mouse_position):
+					_floor_level.reserve_tile_at_global_position(mouse_position)
+				elif _floor_level.is_tile_at_global_position_reserved(mouse_position):
+					_floor_level.unreserve_tile_at_global_position(mouse_position)
+			elif Input.is_action_just_pressed("right_click"):
+				_floor_level.register_reserved_tiles_as_apartment()
+			elif Input.is_action_just_pressed("middle_click"):
+				_mode = Mode.LEVEL_IDLE
+		_:
+			pass
 	
 	return true
+
+func mode_to_string(mode: Mode) -> String:
+	match _mode:
+		Mode.MENU:
+			return "Mode.MENU"
+		Mode.LEVEL_IDLE:
+			return "LevelMode.IDLE"
+		Mode.LEVEL_SELECTING_TILES:
+			return "LevelMode.SELECTING_TILES"
+		_:
+			pass
+	
+	return "Invalid mode."
+
+func print_mode() -> void:
+	if OS.is_debug_build():
+		print("Input manager mode: %s" % mode_to_string(_mode))
 
 ########## Node methods. ##########
 
 func _ready() -> void:
-	pass
+	print_mode()
 
 func _process(delta: float) -> void:
 	match _mode:
 		Mode.MENU:
 			pass
-		Mode.LEVEL:
+		Mode.LEVEL_IDLE:
+			_process_level_input()
+		Mode.LEVEL_SELECTING_TILES:
 			_process_level_input()
 		_:
 			pass
