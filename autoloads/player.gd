6a32c1cffb2ea3ede7c3b6947823b45a074ba86d
CrumblingAway <antonkid@gmail.com>
2024-12-20 22:45:25 +0200

    Player: Add player autoload draft.

---
+++ autoloads/player.gd
@@ -0,0 +1,20 @@
+extends Node
+
+########## Fields. ##########
+
+var money : int:
+	get:
+		return money
+	set(new_money):
+		money = new_money
+
+########## Player methods. ##########
+
+########## Node methods. ##########
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
