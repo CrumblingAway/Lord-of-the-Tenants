fb6fe5b49b74fbc7ddf169ead1f7f7d57c10fb25
CrumblingAway <antonkid@gmail.com>
2024-12-23 20:55:02 +0200

    Utils: Declare default value for callable.

--- utils/utils.gd
+++ utils/utils.gd
@@ -1,10 +1,11 @@
 class_name Utils extends Node
 
-static func printdbg(message: String, callable: Callable) -> void:
+static func printdbg(message: String, callable: Callable = func(): return "") -> void:
 	if not OS.is_debug_build():
 		return
 	
-	if callable:
-		print(message % callable.call())
+	var called_message : String = callable.call()
+	if called_message:
+		print(message % called_message)
 	else:
 		print(message)
