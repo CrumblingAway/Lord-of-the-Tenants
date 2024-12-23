50b62aaae77e55a5a99c3ea134d3fb01883127d7
CrumblingAway <antonkid@gmail.com>
2024-12-23 21:40:28 +0200

    Utils: Require Array as return of callable.

--- utils/utils.gd
+++ utils/utils.gd
@@ -1,11 +1,11 @@
 class_name Utils extends Node
 
-static func printdbg(message: String, callable: Callable = func(): return "") -> void:
+static func printdbg(message: String, callable: Callable = func(): return []) -> void:
 	if not OS.is_debug_build():
 		return
 	
-	var called_message : String = callable.call()
-	if called_message:
+	var called_message : Array = callable.call()
+	if called_message.size() > 0:
 		print(message % called_message)
 	else:
 		print(message)
